0b2766e9b50a90c504446e34795c0792fe19f105
hassan <hassanarshad115@gmail.com>
2017-10-01 13:30:13 -0600

    Add project files.

---
+++ Sqlite/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Sqlite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nametextBox = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.emailtextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.mobiletextBox = new System.Windows.Forms.TextBox();
+            this.savebutton = new System.Windows.Forms.Button();
+            this.closebutton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // nametextBox
+            // 
+            this.nametextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nametextBox.Location = new System.Drawing.Point(96, 17);
+            this.nametextBox.Name = "nametextBox";
+            this.nametextBox.Size = new System.Drawing.Size(161, 23);
+            this.nametextBox.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(263, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(450, 222);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Email:";
+            // 
+            // emailtextBox
+            // 
+            this.emailtextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailtextBox.Location = new System.Drawing.Point(96, 60);
+            this.emailtextBox.Name = "emailtextBox";
+            this.emailtextBox.Size = new System.Drawing.Size(161, 23);
+            this.emailtextBox.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Mobile:";
+            // 
+            // mobiletextBox
+            // 
+            this.mobiletextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mobiletextBox.Location = new System.Drawing.Point(96, 103);
+            this.mobiletextBox.Name = "mobiletextBox";
+            this.mobiletextBox.Size = new System.Drawing.Size(161, 23);
+            this.mobiletextBox.TabIndex = 2;
+            // 
+            // savebutton
+            // 
+            this.savebutton.Location = new System.Drawing.Point(96, 152);
+            this.savebutton.Name = "savebutton";
+            this.savebutton.Size = new System.Drawing.Size(75, 23);
+            this.savebutton.TabIndex = 3;
+            this.savebutton.Text = "Save";
+            this.savebutton.UseVisualStyleBackColor = true;
+            this.savebutton.Click += new System.EventHandler(this.savebutton_Click);
+            // 
+            // closebutton
+            // 
+            this.closebutton.Location = new System.Drawing.Point(182, 152);
+            this.closebutton.Name = "closebutton";
+            this.closebutton.Size = new System.Drawing.Size(75, 23);
+            this.closebutton.TabIndex = 4;
+            this.closebutton.Text = "Close";
+            this.closebutton.UseVisualStyleBackColor = true;
+            this.closebutton.Click += new System.EventHandler(this.closebutton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(263, 2);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(450, 10);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 240);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.closebutton);
+            this.Controls.Add(this.savebutton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.mobiletextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.emailtextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nametextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nametextBox;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox emailtextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox mobiletextBox;
+        private System.Windows.Forms.Button savebutton;
+        private System.Windows.Forms.Button closebutton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Sqlite/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sqlite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void closebutton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void savebutton_Click(object sender, EventArgs e)
+        {
+
+            if (IsValid())
+            {
+
+
+                SQLiteConnection conn = new SQLiteConnection("Data Source=test.db;Version=3;");
+
+
+                string commandstring = "insert into Contacts([Name],[Email],[Mobile]) values(@name,@email,@mobile)";
+                SQLiteCommand cmd = new SQLiteCommand(commandstring, conn);
+
+                conn.Open();
+
+                cmd.Parameters.AddWithValue("@name", nametextBox.Text);
+                cmd.Parameters.AddWithValue("@email", emailtextBox.Text);
+                cmd.Parameters.AddWithValue("@mobile", mobiletextBox.Text);
+
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Save Data Successfully", "SAVE", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                DataGridViewMaDataDekhna();
+
+
+                nametextBox.Clear();
+                emailtextBox.Clear();
+                mobiletextBox.Clear();
+                nametextBox.Focus();
+                
+            }
+
+        }
+
+        private bool IsValid()
+        {
+            if (nametextBox.Text.Trim() == string.Empty)
+            {
+
+                int i;
+
+                progressBar1.Minimum = 0;
+                progressBar1.Maximum = 200;
+
+                for (i = 0; i <= 200; i++)
+                {
+                    progressBar1.Value = i;
+                }
+                MessageBox.Show("Name text box is not fill", "Error !!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                nametextBox.Clear();
+                nametextBox.Focus();
+                return false;
+            }
+            if(emailtextBox.Text.Trim()== string.Empty)
+            {
+                MessageBox.Show("Email text box is not fill", "Error !!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                emailtextBox.Focus();
+                return false;
+            }
+            if (mobiletextBox.Text.Trim() == string.Empty)
+            {
+                MessageBox.Show("Mobile text box is not fill", "Error !!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                mobiletextBox.Focus();
+                return false;
+            }
+            return true;
+        }
+
+
+
+
+
+
+        private void IfDatabaseExists()
+        {
+            ///agr database ni ha to ye database bnayga khudi r table b\
+            ///
+            if(!File.Exists("test.db")){
+                //SQL bra likhna ha jsy SQLiteConnection likha ha asy likhna ha
+                //agr database file ni ha to ye bnady ga r name ak jsa hona chaia uper r nechy
+                
+                SQLiteConnection.CreateFile("test.db");
+
+                SQLiteConnection conn = new SQLiteConnection("Data Source=test.db;Version=3;");
+
+
+                string commandstring = "CREATE TABLE Contacts(Name NVARCHAR(50), Email NVARCHAR(50), Mobile NVARCHAR(50))";
+                 SQLiteCommand cmd = new SQLiteCommand(commandstring, conn);
+
+                conn.Open();
+
+                cmd.ExecuteNonQuery();
+
+            }
+            else //agr database phly he mojod ha to ye kam ho
+            {
+                DataGridViewMaDataDekhna();
+            }
+
+
+
+        }
+
+        private void DataGridViewMaDataDekhna()
+        {
+            dataGridView1.DataSource = GetData();
+        }
+
+        private DataTable GetData()
+        {
+            DataTable dt = new DataTable();
+
+            //class bna k database ko accesskrna ha
+            SQLiteConnection conn = new SQLiteConnection("Data Source=test.db;Version=3;");
+
+
+            string commandstring = " select * from Contacts";
+            SQLiteCommand cmd = new SQLiteCommand(commandstring, conn);
+
+            conn.Open();
+
+            SQLiteDataReader reader = cmd.ExecuteReader();
+            dt.Load(reader);
+
+
+            return dt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IfDatabaseExists();
+            
+        }
+    }
+}
